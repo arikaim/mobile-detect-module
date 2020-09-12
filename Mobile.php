e4b386f8c0ae9c970ae4e0d0f9142a7ebb5a8ab2
Arikaim <sales@intersoft.biz>
2020-09-12 17:08:08 +0300

    =

---
+++ Mobile.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Arikaim
+ *
+ * @link        http://www.arikaim.com
+ * @copyright   Copyright (c)  Konstantin Atanasov <info@arikaim.com>
+ * @license     http://www.arikaim.com/license
+ * 
+*/
+namespace Arikaim\Modules\Mobile;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+use Arikaim\Core\Extension\Module;
+use Arikaim\Core\Utils\Mobile as MobileUtils;
+
+/**
+ * Mobile detect middleware class
+ */
+class Mobile extends Module implements MiddlewareInterface
+{
+    /**
+     * Process middleware
+     * 
+     * @param ServerRequestInterface  $request
+     * @param RequestHandlerInterface $handler
+     * @return ResponseInterface
+    */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {       
+        $request = $request->withAttribute('mobile',MobileUtils::mobile());   
+      
+        return $handler->handle($request);
+    }
+}
